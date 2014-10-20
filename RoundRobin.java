a87c19c07ef9ea4a53f8bc01b707939d1a1b574a
Paul Chiappetta <paulc183@gmail.com>
2014-10-20 19:51:25 -0400

    Starting RR

--- RoundRobin.java
+++ RoundRobin.java
@@ -3,7 +3,12 @@ import java.util.PriorityQueue;
 
 //with preemption via configurable time slice TSLICE=100MS
 class RoundRobin extends Algorithm{
-	public RoundRobin(Process[] p_, int m_){
+
+	public int t_slice;
+
+	public RoundRobin(Process[] p_, int m_, int t_sl){
 		super(p_,m_);
+		super.readyQueue = new PriorityQueue<>(super.processes.length, super.burstComparator);
+		t_slice = t_sl;
 	}
 }
